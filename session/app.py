9d36a8e3462395cfe0c830f96416e54c509f7760
Suresh Sonwane <sonwanesuresh95@gmail.com>
2022-09-25 14:12:59 +0530

    Add files via upload

--- session/app.py
+++ session/app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, session, render_template
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'lionel messi'
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    # adding session variable
-    session['username'] = 'surie'
-    return f"welcome {session['username']} !"+"<br><br><a href='/logout'>logout</a>"
+    if request.method == 'POST':
+        # adding session variable
+        session['username'] = request.form['username']
+        return f"welcome {session['username']} !"+"<br><br><a href='/logout'>logout</a>"
+    return render_template('t1.html')
 
 @app.route('/logout')
 def logout():
+    # removing session variable
     session.pop('username', None)
     return "you are logged out, <a href='/login'>click here to login</a>"
 
